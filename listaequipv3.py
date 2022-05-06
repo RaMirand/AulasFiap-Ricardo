9cb88dcf9e30ed6c3ef8a53ed3f6b1a34c835753
Ricardo Miranda <portugamiranda@gmail.com>
2022-05-06 15:19:46 -0300

    Adicionadas funcoes ao codigo

--- listaequipv3.py
+++ listaequipv3.py
@@ -2,6 +2,7 @@ inventario = [
     ["Impressora", 349.99, 1111, "ENG"], ["Scanner", 147.59, 2222, "ENG"],
     ["Notebook01", 4389.79, 1234, "TI"], ["Notebook02", 4389.79, 1235, "TI"]
 ]
+valores = []
 
 for elemento in inventario:
     print("Nome........: ", elemento[0])
@@ -9,3 +10,10 @@ for elemento in inventario:
     print("Serial......: ", elemento[2])
     print("Departamento: ", elemento[3])
     print("")
+
+for elemento in inventario:
+    valores.append(elemento[1])
+if len(valores) > 0:
+    print("O equipamento mais caro em estoque custou R${}".format(max(valores)))
+    print("O equipamento mais barato em estoque custou R${}".format(min(valores)))
+    print("O valor total em equipamentos em estoque é de R${}".format(sum(valores)))
