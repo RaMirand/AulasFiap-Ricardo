820465b2dceb9dd6ecaafd24cfe225d9f9ca939c
Ricardo Miranda <portugamiranda@gmail.com>
2022-05-06 11:55:26 -0300

    commit inicial

---
+++ funcoes.py
@@ -0,0 +1,21 @@
+def busca(lista):
+    busca = input("Digite o nome do equipamento que deseja buscar: ")
+    for elemento in lista:
+        if busca == elemento[0]:
+            print("Equipamento..: ", elemento[0])
+            print("Valor........: ", elemento[1])
+            print("Serial.......: ", elemento[2])
+            print("Departamento.: ", elemento[3])
+        else:
+            print("Não foi encontrado nenhum equipamento com este nome!")
+
+
+def depreciar(lista, porc):
+    depreciacao = input("Digite o nome do equipamento que será depreciado: ")
+    for elemento in lista:
+        if depreciacao == elemento[0]:
+            print("Valor antigo: ", elemento[1])
+            elemento[1] = elemento[1] * (1-porc/100)
+            print("Novo valor: ", elemento[1])
+        else:
+            print("Não foi encontrado nenhum equipamento com este nome!")
--- listaequipv2.py
+++ listaequipv2.py
@@ -18,3 +18,13 @@ for indice in range(0, len(equipamentos)):
     print("Serial.......:", seriais[indice])
     print("Departamento.:", departamentos[indice])
     print("")
+
+busca = input("Digite o nome do equipamento que deseja buscar: ")
+for indice in range(0, len(equipamentos)):
+    if busca == equipamentos[indice]:
+        print("Equipamento..: ", equipamentos[indice])
+        print("Valor........: ", valores[indice])
+        print("Serial.......: ", seriais[indice])
+        print("Departamento.: ", departamentos[indice])
+    else:
+        print("Não foi encontrado nenhum equipamento com este nome!")
